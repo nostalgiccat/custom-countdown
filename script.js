08041ed6d55e7dba76561eb6281b3b804e829da9
nostalgiccat <nesdom27@gmail.com>
2023-03-04 22:52:39 -0700

    modified js script

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm'); 
 const dateEL = document.getElementById('date-picker');
 
+let countdownTitle = ''; 
+let countdownDate = ''; 
+
+
 // Set Date Input Min with Today's date
 const today = new Date().toISOString().split('T')[0];
 dateEL.setAttribute('min', today); 
+
+// Take Value from Form Input
+function updateCountdown(e) {
+    e.preventDefault(); 
+    countdownTitle = e.srcElement[0].value; 
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate); 
+}
+
+// Event Listener
+countdownForm.addEventListener('submit', updateCountdown)
